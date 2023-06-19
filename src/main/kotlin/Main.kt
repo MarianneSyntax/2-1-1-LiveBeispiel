1a58ea6185454063e12b03133efb481c61a5a2de
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-06-19 10:41:20 +0100

    Beispiel für einen Laufzeitfehler hinzugefügt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,10 @@ fun main() {
     println("Hello World!")
 
     // kompilierfehler:
-    val x: Int = "Hallo" // Fehler: Typenunverträglichkeit
-    println(x)
+    //val x: Int = "Hallo" // Fehler: Typenunverträglichkeit
+    //println(x)
 
-
+    // laufzeitfehler:
+    val numbersList = listOf(1,2,3)
+    println(numbersList[4])
 }
